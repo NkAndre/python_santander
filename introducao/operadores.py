3a7d6cd497b9a7447e2b10cbd84ec0b6b91b691f
NkAndre <andre270708@gmail.com>
2026-08-14 14:39:43 -0300

    operadores basicos sabidos

---
+++ introducao/operadores.py
@@ -0,0 +1,25 @@
+## Aritméticos
+
+a = 10
+
+b = 3
+
+print(f'SOMA = ',a+b)
+
+print(f'SUBTRAÇÃO = ',a-b)
+
+print(f'MULTIPLICAÇÃO = ',a*b)
+
+print(f'DIVISÃO = ',a/b)
+
+print(f'DIVISÃO INTEIRA = ',a//b)
+
+print(f'MODULO = ',a%b)
+
+print(f'EXPONENCIAÇÃO = ',a**b)
+
+##LOGICOS
+
+resultado_and = (a > 5) and (b < 5)   # True
+resultado_or = (a > 15) or (b < 5)   # True
+resultado_not = not (a > 5)   # False
