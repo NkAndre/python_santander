4389f8acf771af2532fc972f7fbc777e004c5be4
NkAndre <andre270708@gmail.com>
2026-08-14 14:43:29 -0300

    estrutura de condicionais finalizada

---
+++ estruturaControle/condicionais.py
@@ -0,0 +1,20 @@
+idade = 15
+if idade >=18:
+    print('Você é maior de idade')
+else:
+    print('Você é menor de idade')
+
+print('--' *20)
+
+## IF-ELIF-ELSE
+
+nota = 85
+
+if nota >=90:
+    print('Excelente !')
+elif nota>=80:
+    print('Muito Bom')
+elif nota >=70:
+    print('Bom')
+else:
+    print('Precisa melhorar')
