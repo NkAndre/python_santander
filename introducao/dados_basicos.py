5c94ece95424c237ed821c130f1e56899c04b552
NkAndre <andre270708@gmail.com>
2026-08-14 14:29:11 -0300

    feat:dados basicos feitos

---
+++ introducao/dados_basicos.py
@@ -0,0 +1,14 @@
+## Strings e inteiro
+idade = 18
+nome = 'André'
+
+print(f'Eu me chamo {nome} e tenho {idade} anos')
+
+## Tipos flutuantes
+
+preco = 9.99
+altura = 1.81
+
+## Boolean 
+is_maior_de_idade = True
+tem_desconto = False
