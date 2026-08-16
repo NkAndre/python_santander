4e07ca575199861d6e2367280055fe7aed9d6d3c
NkAndre <andre270708@gmail.com>
2026-08-16 14:59:33 -0300

    ate no domingo eu codo

---
+++ funcoes/hello.py
@@ -0,0 +1,11 @@
+print('Sem parametro')
+def mensagem():
+    print('Hello world')
+mensagem()
+
+print('-' *20)
+print('Com parametro')
+
+def saudacao(nome):
+    print(f'Olá , {nome}')
+saudacao('André')
